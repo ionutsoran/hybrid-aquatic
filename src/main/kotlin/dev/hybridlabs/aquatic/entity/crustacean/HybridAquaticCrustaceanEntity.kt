858f1cbbc7426e7dfb7f12bd842d8e1493ee0de8
MysticKoko <132279944+MysticKoko@users.noreply.github.com>
2024-11-25 06:25:56 +0200

    Started altering crustacean behaviour

--- src/main/kotlin/dev/hybridlabs/aquatic/entity/crustacean/HybridAquaticCrustaceanEntity.kt
+++ src/main/kotlin/dev/hybridlabs/aquatic/entity/crustacean/HybridAquaticCrustaceanEntity.kt
@@ -5,7 +5,6 @@ import dev.hybridlabs.aquatic.entity.fish.HybridAquaticFishEntity
 import dev.hybridlabs.aquatic.entity.fish.ray.HybridAquaticRayEntity
 import dev.hybridlabs.aquatic.entity.shark.HybridAquaticSharkEntity
 import dev.hybridlabs.aquatic.tag.HybridAquaticBlockTags
-import dev.hybridlabs.aquatic.tag.HybridAquaticItemTags
 import net.minecraft.block.Blocks
 import net.minecraft.entity.EntityData
 import net.minecraft.entity.EntityGroup
@@ -13,29 +12,35 @@ import net.minecraft.entity.EntityType
 import net.minecraft.entity.SpawnReason
 import net.minecraft.entity.ai.control.MoveControl
 import net.minecraft.entity.ai.goal.EscapeDangerGoal
-import net.minecraft.entity.ai.goal.LookAroundGoal
-import net.minecraft.entity.ai.goal.TemptGoal
+import net.minecraft.entity.ai.goal.FleeEntityGoal
 import net.minecraft.entity.ai.goal.WanderAroundGoal
 import net.minecraft.entity.ai.pathing.EntityNavigation
+import net.minecraft.entity.ai.pathing.MobNavigation
 import net.minecraft.entity.ai.pathing.PathNodeType
 import net.minecraft.entity.damage.DamageSource
 import net.minecraft.entity.data.DataTracker
 import net.minecraft.entity.data.TrackedData
 import net.minecraft.entity.data.TrackedDataHandlerRegistry
 import net.minecraft.entity.mob.WaterCreatureEntity
+import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.nbt.NbtCompound
-import net.minecraft.recipe.Ingredient
 import net.minecraft.registry.tag.TagKey
 import net.minecraft.sound.SoundEvent
 import net.minecraft.sound.SoundEvents
 import net.minecraft.util.math.BlockPos
 import net.minecraft.util.math.random.Random
-import net.minecraft.world.*
+import net.minecraft.world.LocalDifficulty
+import net.minecraft.world.ServerWorldAccess
+import net.minecraft.world.World
+import net.minecraft.world.WorldAccess
 import net.minecraft.world.biome.Biome
 import software.bernie.geckolib.animatable.GeoEntity
 import software.bernie.geckolib.constant.DefaultAnimations
 import software.bernie.geckolib.core.animatable.instance.AnimatableInstanceCache
-import software.bernie.geckolib.core.animation.*
+import software.bernie.geckolib.core.animation.AnimatableManager
+import software.bernie.geckolib.core.animation.AnimationController
+import software.bernie.geckolib.core.animation.AnimationState
+import software.bernie.geckolib.core.animation.EasingType
 import software.bernie.geckolib.util.GeckoLibUtil
 
 @Suppress("DEPRECATION", "LeakingThis", "UNUSED_PARAMETER")
@@ -49,7 +54,6 @@ open class HybridAquaticCrustaceanEntity(
     open val collisionRules: List<HybridAquaticFishEntity.VariantCollisionRules> = listOf(),
 ) : WaterCreatureEntity(type, world), GeoEntity {
     private val factory = GeckoLibUtil.createInstanceCache(this)
-    private var landNavigation: EntityNavigation = createNavigation(world)
     private var fromFishingNet = false
 
     var size: Int
@@ -90,10 +94,9 @@ open class HybridAquaticCrustaceanEntity(
 
     override fun initGoals() {
         super.initGoals()
-        goalSelector.add(0, EscapeDangerGoal(this, 1.0))
-        goalSelector.add(1, TemptGoal(this, 0.5, Ingredient.fromTag(HybridAquaticItemTags.CRUSTACEAN_TEMPT_ITEMS), true))
+        goalSelector.add(1, EscapeDangerGoal(this, 1.0))
+        goalSelector.add(2, FleeEntityGoal(this, PlayerEntity::class.java, 8.0f, 1.0, 1.0))
         goalSelector.add(3, WanderAroundGoal(this, 0.4))
-        goalSelector.add(5, LookAroundGoal(this))
     }
 
     override fun initialize(
@@ -159,7 +162,7 @@ open class HybridAquaticCrustaceanEntity(
         }
 
         moveControl = MoveControl(this)
-        navigation = this.landNavigation
+        navigation = MobNavigation(this, world)
         stepHeight = 1.0F
     }
 
@@ -217,6 +220,10 @@ open class HybridAquaticCrustaceanEntity(
 
     override fun getDeathSound(): SoundEvent {
         return SoundEvents.ENTITY_TURTLE_EGG_BREAK
+    }
+
+    override fun createNavigation(world: World): EntityNavigation {
+        return MobNavigation(this, world)
     }
 
     // endregion
