f31c9dd505b35ed6afbd9416addd8aa6fc239c8b
MysticKoko <132279944+MysticKoko@users.noreply.github.com>
2024-11-30 16:42:13 +0200

    Added shadows to crustaceans

--- src/client/kotlin/dev/hybridlabs/aquatic/client/render/entity/crustacean/HybridAquaticCrustaceanEntityRenderer.kt
+++ src/client/kotlin/dev/hybridlabs/aquatic/client/render/entity/crustacean/HybridAquaticCrustaceanEntityRenderer.kt
@@ -6,6 +6,10 @@ import software.bernie.geckolib.model.GeoModel
 import software.bernie.geckolib.renderer.GeoEntityRenderer
 
 open class HybridAquaticCrustaceanEntityRenderer<T: HybridAquaticCrustaceanEntity>(context: EntityRendererFactory.Context, model: GeoModel<T>): GeoEntityRenderer<T>(context, model) {
+    init {
+        this.shadowRadius = 0.3f
+    }
+
     override fun getMotionAnimThreshold(animatable: T): Float {
         return 0.0025f
     }
