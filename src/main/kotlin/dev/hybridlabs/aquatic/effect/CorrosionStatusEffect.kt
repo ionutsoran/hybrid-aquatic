991a145e37fb94b409f91f1497aa7b48ddaa0882
MysticKoko <132279944+MysticKoko@users.noreply.github.com>
2024-11-30 02:47:11 +0200

    Made corrosion affect entities instead of only players

--- src/main/kotlin/dev/hybridlabs/aquatic/effect/CorrosionStatusEffect.kt
+++ src/main/kotlin/dev/hybridlabs/aquatic/effect/CorrosionStatusEffect.kt
@@ -4,40 +4,36 @@ import net.minecraft.entity.EquipmentSlot
 import net.minecraft.entity.LivingEntity
 import net.minecraft.entity.effect.StatusEffect
 import net.minecraft.entity.effect.StatusEffectCategory
-import net.minecraft.entity.player.PlayerEntity
-import net.minecraft.item.ArmorItem
 
 class CorrosionStatusEffect : StatusEffect(StatusEffectCategory.HARMFUL, 0x9d9136) {
 
     override fun applyUpdateEffect(entity: LivingEntity, amplifier: Int) {
         if (entity.world.isClient) return
-        if (entity is PlayerEntity) {
-            corrodeTool(entity)
-            corrodeArmor(entity)
-        }
+        corrodeTool(entity)
+        corrodeArmor(entity)
     }
 
     override fun canApplyUpdateEffect(duration: Int, amplifier: Int): Boolean {
         return duration % 20 == 0
     }
 
-    private fun corrodeTool(player: PlayerEntity) {
-        val mainHandStack = player.mainHandStack
+    private fun corrodeTool(entity: LivingEntity) {
+        val mainHandStack = entity.mainHandStack
         if (mainHandStack.isDamageable) {
-            mainHandStack.damage(1, player) { it.sendToolBreakStatus(player.activeHand) }
+            mainHandStack.damage(1, entity) { it.sendToolBreakStatus(entity.activeHand) }
         }
 
-        val offHandStack = player.offHandStack
+        val offHandStack = entity.offHandStack
         if (offHandStack.isDamageable) {
-            offHandStack.damage(1, player) { it.sendToolBreakStatus(player.activeHand) }
+            offHandStack.damage(1, entity) { it.sendToolBreakStatus(entity.activeHand) }
         }
     }
 
-    private fun corrodeArmor(player: PlayerEntity) {
+    private fun corrodeArmor(entity: LivingEntity) {
         for (slot in EquipmentSlot.entries) {
-            val armorStack = player.getEquippedStack(slot)
+            val armorStack = entity.getEquippedStack(slot)
             if (armorStack.isDamageable) {
-                armorStack.damage(1, player) { it.sendEquipmentBreakStatus(slot) }
+                armorStack.damage(1, entity) { it.sendEquipmentBreakStatus(slot) }
             }
         }
     }
