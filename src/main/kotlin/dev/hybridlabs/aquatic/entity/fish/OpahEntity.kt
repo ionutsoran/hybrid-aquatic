28929fced3f0a615cd8313556bba7e1b2779f1a2
MysticKoko <132279944+MysticKoko@users.noreply.github.com>
2024-12-02 03:56:05 +0200

    Opah will follow tuna as they do irl

--- src/main/kotlin/dev/hybridlabs/aquatic/entity/fish/OpahEntity.kt
+++ src/main/kotlin/dev/hybridlabs/aquatic/entity/fish/OpahEntity.kt
@@ -2,8 +2,10 @@ package dev.hybridlabs.aquatic.entity.fish
 
 import dev.hybridlabs.aquatic.tag.HybridAquaticEntityTags
 import net.minecraft.entity.EntityType
+import net.minecraft.entity.ai.goal.Goal
 import net.minecraft.entity.attribute.DefaultAttributeContainer
 import net.minecraft.entity.attribute.EntityAttributes
+import net.minecraft.entity.mob.PathAwareEntity
 import net.minecraft.entity.mob.WaterCreatureEntity
 import net.minecraft.world.World
 
@@ -20,6 +22,11 @@ class OpahEntity(entityType: EntityType<out OpahEntity>, world: World) :
         return 2
     }
 
+    override fun initGoals() {
+        goalSelector.add(2, FollowTunaGoal(this, 1.5, 4.0F, 8.0F))
+        super.initGoals()
+    }
+
     companion object {
         fun createMobAttributes(): DefaultAttributeContainer.Builder {
             return WaterCreatureEntity.createMobAttributes()
@@ -27,6 +34,49 @@ class OpahEntity(entityType: EntityType<out OpahEntity>, world: World) :
                 .add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.7)
                 .add(EntityAttributes.GENERIC_ATTACK_DAMAGE, 2.0)
                 .add(EntityAttributes.GENERIC_FOLLOW_RANGE, 12.0)
+        }
+    }
+
+    internal class FollowTunaGoal(
+        private val mob: PathAwareEntity,
+        private val speed: Double,
+        private val minDistance: Float,
+        private val maxDistance: Float
+    ) : Goal() {
+
+        private var target: TunaEntity? = null
+
+        override fun canStart(): Boolean {
+            val closestTuna = mob.world.getEntitiesByClass(
+                TunaEntity::class.java,
+                mob.boundingBox.expand(maxDistance.toDouble())
+            ) { true }
+                .minByOrNull { it.squaredDistanceTo(mob) }
+
+            if (closestTuna != null && closestTuna.squaredDistanceTo(mob) > (minDistance * minDistance)) {
+                target = closestTuna
+                return true
+            }
+            return false
+        }
+
+        override fun shouldContinue(): Boolean {
+            return target != null && target!!.isAlive && mob.squaredDistanceTo(target!!) > (minDistance * minDistance)
+        }
+
+        override fun start() {
+            mob.navigation.startMovingTo(target, speed)
+        }
+
+        override fun stop() {
+            target = null
+            mob.navigation.stop()
+        }
+
+        override fun tick() {
+            target?.let {
+                mob.navigation.startMovingTo(it, speed)
+            }
         }
     }
 }
