a39fc7e300c597f62941531c8fe5bbcfa385e146
MysticKoko <132279944+MysticKoko@users.noreply.github.com>
2024-12-13 22:50:07 +0200

    Cleanup

--- src/client/kotlin/dev/hybridlabs/aquatic/client/model/entity/shark/FrilledSharkEntityModel.kt
+++ src/client/kotlin/dev/hybridlabs/aquatic/client/model/entity/shark/FrilledSharkEntityModel.kt
@@ -2,5 +2,4 @@ package dev.hybridlabs.aquatic.client.model.entity.shark
 
 import dev.hybridlabs.aquatic.entity.shark.HybridAquaticSharkEntity
 
-class FrilledSharkEntityModel : HybridAquaticSharkEntityModel<HybridAquaticSharkEntity>("frilled_shark") {
-}
+class FrilledSharkEntityModel : HybridAquaticSharkEntityModel<HybridAquaticSharkEntity>("frilled_shark")
