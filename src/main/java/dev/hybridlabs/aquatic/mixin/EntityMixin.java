071d810d15bfc2fe5c0474a41ff9307b631dc3d4
Ivan-Khar <47220198+Ivan-Khar@users.noreply.github.com>
2024-12-18 08:22:50 +0300

    Changed Mixin Injection point

--- src/main/java/dev/hybridlabs/aquatic/mixin/EntityMixin.java
+++ src/main/java/dev/hybridlabs/aquatic/mixin/EntityMixin.java
@@ -3,51 +3,68 @@ package dev.hybridlabs.aquatic.mixin;
 import dev.hybridlabs.aquatic.effect.HybridAquaticStatusEffects;
 import dev.hybridlabs.aquatic.item.HybridAquaticItems;
 import dev.hybridlabs.aquatic.tag.HybridAquaticFluidTags;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.effect.StatusEffectInstance;
-import net.minecraft.fluid.Fluid;
 import net.minecraft.fluid.FluidState;
-import net.minecraft.registry.tag.TagKey;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Box;
-import net.minecraft.util.math.Vec3d;
+import net.minecraft.util.math.MathHelper;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(Entity.class)
+@Mixin(LivingEntity.class)
 public abstract class EntityMixin {
 
     @Unique
-    private int brineEffectTick = 100;
+    private int brineEffectTick = 0;
     
     @Inject(
-        method = "updateMovementInFluid",
+        method = "baseTick",
         at = @At(
             value = "INVOKE",
-            target = "Lnet/minecraft/fluid/FluidState;isIn(Lnet/minecraft/registry/tag/TagKey;)Z"
-        ),
-        locals = LocalCapture.CAPTURE_FAILSOFT
+            target = "Lnet/minecraft/entity/LivingEntity;isSubmergedIn(Lnet/minecraft/registry/tag/TagKey;)Z"
+        )
     )
-    private void applyToxicShockInBrine(TagKey<Fluid> tag, double speed, CallbackInfoReturnable<Boolean> cir, Box box, int i, int j, int k, int l, int m, int n, double d, boolean bl, boolean bl2, Vec3d vec3d, int o, BlockPos.Mutable mutable, int p, int q, int r, FluidState fluidState) {
-        if ((Object) this instanceof LivingEntity entity) {
+    private void applyToxicShockInBrine(CallbackInfo ci) {
+        LivingEntity livingEntity = (LivingEntity) (Object) this;
+        
+        if (brineEffectTick >= 20) {
+            brineEffectTick = 0;
+            Box box = livingEntity.getBoundingBox().contract(0.001);
+            int entityMinX = MathHelper.floor(box.minX);
+            int entityMaxX = MathHelper.ceil(box.maxX);
+            int entityMinY = MathHelper.floor(box.minY);
+            int entityMaxY = MathHelper.ceil(box.maxY);
+            int entityMinZ = MathHelper.floor(box.minZ);
+            int entityMaxZ = MathHelper.ceil(box.maxZ);
+            BlockPos.Mutable mutable = new BlockPos.Mutable();
             
-            if (fluidState.isIn(HybridAquaticFluidTags.INSTANCE.getBRINE())) {
-                if (brineEffectTick >= 100) {
-                    if (!isWearingDivingSet(entity)) entity.addStatusEffect(new StatusEffectInstance(HybridAquaticStatusEffects.INSTANCE.getTOXIC_SHOCK(), 100, 0));
-                    entity.addStatusEffect(new StatusEffectInstance(HybridAquaticStatusEffects.INSTANCE.getCORROSION(), 100, 0));
-                    
-                    brineEffectTick = 0;
+            for(int entityX = entityMinX; entityX < entityMaxX; ++entityX) {
+                for(int entityY = entityMinY; entityY < entityMaxY; ++entityY) {
+                    for (int entityZ = entityMinZ; entityZ < entityMaxZ; ++entityZ) {
+                        mutable.set(entityX, entityY, entityZ);
+                        FluidState fluidState = livingEntity.getWorld().getFluidState(mutable);
+                        
+                        if(fluidState.isIn(HybridAquaticFluidTags.INSTANCE.getBRINE())) tickBrine();
+                    }
                 }
-                
-                brineEffectTick++;
             }
+            
         }
+        
+        brineEffectTick++;
+    }
+    
+    @Unique
+    private void tickBrine() {
+        LivingEntity livingEntity = (LivingEntity) (Object) this;
+        
+        if (!isWearingDivingSet(livingEntity)) livingEntity.addStatusEffect(new StatusEffectInstance(HybridAquaticStatusEffects.INSTANCE.getTOXIC_SHOCK(), 100, 0));
+        livingEntity.addStatusEffect(new StatusEffectInstance(HybridAquaticStatusEffects.INSTANCE.getCORROSION(), 100, 0));
     }
     
     @Unique
